ab9a67a014858eaee410841c8e2cfa33bdbd8045
yogashreeck <ykumarostb1@gmail.com>
2019-07-26 16:15:37 +0530

    search data displayed

--- src/components/StudentComponent.js
+++ src/components/StudentComponent.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import { studentProfile } from './UserFunction';
-import StudentDetails from './StudentDetails'
+// import StudentDetails from './StudentDetails'
+
+
 
 class StudentComponent extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       studentname: '',
+      course: '',
       address: '',
       email: '',
       mobileNumber: '',
-      items: [],
+      repos: [],
       submitted: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    debugger;
-    fetch('http://localhost:8000/users/studentProfile')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({  items: data })
-      console.log(this.state. items)
-    })
-    .catch(console.log('error'))  
-}
+
+  handleSearch = (user) => {
+    let url = 'http://localhost:8000/users/studentProfile';
+    fetch(url).
+      then(response => response.json()).then((repos) => {
+        console.log(repos);
+        console.log(repos.length);
+        this.setState({
+          repos: repos
+        });
+      });
+  };
+
 
   handleChange(e) {
     // const { name, value } = e.target;
@@ -50,31 +56,38 @@ class StudentComponent extends Component {
       }
     })
   }
+
   render() {
-    const { studentname, address, email, mobileNumber, submitted } = this.state;
+    const { studentname,course, address, email, mobileNumber, submitted } = this.state;
     return (
       <div>
-         <StudentDetails items={this.state.items}/>
         <div className="conatiner">
-
           <div className="row">
             <form action="#" className=" form col-md-3 offset-md-9">
               <div class="input-group ">
-                <input type="text" class="form-control" placeholder='Enter Student Name' />
-                <div className="input-group-append">
-                  <button class="btn btn-primary" type="button">Search</button>
-                </div>
+                {/* <input handleSubmit={this.handleSearch} type="text" class="form-control" 
+              placeholder='Enter Student Name' /> */}
+                <SearchBar handleSubmit={this.handleSearch} />
+                {/* <RepoList repos={this.state.repos} /> */}
               </div>
             </form>
             <div className="col-md-3 offset-md-1 ">
               <h3>Student's Register Form</h3>
               <form className="form" onSubmit={this.handleSubmit}>
                 <div className={'form-group' + (submitted && !studentname ? ' has-error' : '')}>
-                  <label For="student Name">Student Name</label>
+                  <label For="studentName">Student Name</label>
                   <input type="text" className="form-control" name="studentname" value={this.state.studentname}
                     onChange={this.handleChange} />
                   {submitted && !studentname &&
                     <div className="help-block">Student Name is required</div>
+                  }
+                </div>
+                <div className={'form-group' + (submitted && !course ? ' has-error' : '')}>
+                  <label For="course">Course</label>
+                  <input type="text" className="form-control" name="course" value={this.state.course}
+                    onChange={this.handleChange} />
+                  {submitted && !course &&
+                    <div className="help-block">Course is required</div>
                   }
                 </div>
                 <div className={'form-group' + (submitted && !address ? ' has-error' : '')}>
@@ -95,7 +108,8 @@ class StudentComponent extends Component {
                 </div>
                 <div className={'form-group' + (submitted && !mobileNumber ? ' has-error' : '')}>
                   <label For="mobileNumber">Mobile Number</label>
-                  <input type="text" className="form-control" name="mobileNumber" value={this.state.mobileNumber}
+                  <input type="text" className="form-control" name="mobileNumber" 
+                  value={this.state.mobileNumber}
                     onChange={this.handleChange} />
                   {submitted && !mobileNumber &&
                     <div className="help-block">mobile Number is required</div>
@@ -106,11 +120,80 @@ class StudentComponent extends Component {
                 </div>
               </form>
             </div>
+            <div className="col-md-4">
+            <RepoList repos={this.state.repos} />
+            </div>
           </div>
         </div>
       </div>
-      
+
     );
+  }
+
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const text = event.target.text.value;
+    this.props.handleSubmit(text);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className=" form col-md-11 offset-md-1">
+        <div class="input-group ">
+          {/* <input
+            name="text"
+            className="form-control"
+            type="text"
+            placeholder="Type github user and press ENTER"
+          /> */}
+            <input handleSubmit={this.handleSearch}   name="text" type="text" class="form-control" 
+              placeholder='Enter Student Name' />
+          <div className="input-group-append">
+            <button class="btn btn-primary" type="button">Search</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+
+class RepoList extends React.Component {
+
+  render() {
+    var rows = [];
+    this.props.repos.map((repo, index) => rows.push(<RepoItem key={index} repo={repo} />))
+    return (
+      <div className="list-group">
+        {rows}
+      </div>
+    )
+  }
+}
+RepoList.defaultProps = {
+  repos: []
+};
+
+class RepoItem extends React.Component {
+  render() {
+    return (
+      <div className="list-group-item list-group-item-action flex-column align-items-start">
+        <div className="d-flex w-100 justify-content-between">
+          <h5 className="mb-1">{this.props.repo.studentname}</h5>
+        </div>
+        <p>{this.props.repo.email}</p>
+        <p className="mb-1">{this.props.repo.address}</p>
+        <p>{this.props.repo.mobileNumber}</p>
+        <small>{this.props.repo.html_url}</small>
+      </div>
+    )
   }
 }
 
